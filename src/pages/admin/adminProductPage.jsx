7132c9a3ab78999d84b5ce38f3cfecbaecaa3c0e
gthilakshana <gavrawavanniarachchi@gmail.com>
2025-06-25 01:16:03 -0700

    Animate Spin added 🤩

--- src/pages/admin/adminProductPage.jsx
+++ src/pages/admin/adminProductPage.jsx
@@ -7,13 +7,18 @@ import { Link } from "react-router-dom";
 
 export default function AdminProductPage() {
     const [products, setProducts] = useState([]);
+    const [productLoaded, setProductLoaded] = useState(false);
 
     useEffect(() => {
-        axios.get("http://localhost:5000/api/products").then((response) => {
-            console.log(response.data);
-            setProducts(response.data);
-        });
-    }, []);
+        if (!productLoaded) {
+            axios.get("http://localhost:5000/api/products").then((response) => {
+                console.log(response.data);
+                setProducts(response.data);
+                setProductLoaded(true);
+            });
+        }
+
+    }, [productLoaded]);
 
     return (
         <div className="w-full  bg-white rounded-lg overflow-hidden  lg:p-1  flex flex-col items-center relative">
@@ -24,57 +29,60 @@ export default function AdminProductPage() {
             {/* Scrollable Container */}
             <div className="w-full  bg-white shadow-lg rounded-lg overflow-y-auto ">
                 <h1 className="text-2xl font-bold text-gray-800 mb-5 text-center ">Admin Product Page</h1>
+                <div className="w-full h-full flex justify-center items-center">
+                    <div className="w-[40px] h-[40px] border-gray-300 border-[3px] animate-spin border-b-blue-500 rounded-full ">
 
-                <div className="h-[700px] overflow-y-auto "> {/* Vertical Scroll */}
-                    <table className="w-full text-sm text-left text-gray-700">
-                        <thead className="text-xs uppercase bg-blue-200 text-gray-600 sticky top-0">
-                            <tr>
-                                <th className="px-6 py-4">Product ID</th>
-                                <th className="px-6 py-4">Product Name</th>
-                                <th className="px-6 py-4">Price</th>
-                                <th className="px-6 py-4">Last Price</th>
-                                <th className="px-6 py-4">Stock</th>
-                                <th className="px-6 py-4">Description</th>
-                                <th className="px-6 py-4 text-center">Actions</th>
-                            </tr>
-                        </thead>
-                        <tbody>
-                            {products.map((product, index) => (
-                                <tr key={index} className="border-b hover:bg-blue-50 transition duration-300">
-                                    <td className="px-6 py-4">{product.productId}</td>
-                                    <td className="px-6 py-4">{product.productName}</td>
-                                    <td className="px-6 py-4">Rs. {product.price}</td>
-                                    <td className="px-6 py-4">Rs. {product.lastPrice}</td>
-                                    <td className="px-6 py-4">{product.stock}</td>
-                                    <td className="px-6 py-4">{product.description}</td>
-                                    <td className="px-6 py-4 text-center flex gap-4 justify-center">
-                                        <button className="text-red-600 hover:text-red-800"
-                                            title="Delete"
-                                            onClick={() => {
-                                                const token = localStorage.getItem('token');
-                                                axios.delete(`http://localhost:5000/api/products/${product.productId}`, {
-                                                    headers: {
-                                                        Authorization: `Bearer ${token}`,
-                                                    },
-                                                }).then(() => {
-
-                                                    toast.success("Product deleted successfully");
-                                                });
-                                            }}
-                                        >
-                                            <FaTrash />
-                                        </button>
-                                        <button className="text-blue-600 hover:text-blue-800">
-                                            <FaPencil />
-                                        </button>
-                                    </td>
-                                </tr>
-                            ))}
-                        </tbody>
-                    </table>
+                    </div>
                 </div>
             </div>
         </div>
     );
 }
 
+//   <table className="w-full text-sm text-left text-gray-700">
+//                         <thead className="text-xs uppercase bg-blue-200 text-gray-600 sticky top-0">
+//                             <tr>
+//                                 <th className="px-6 py-4">Product ID</th>
+//                                 <th className="px-6 py-4">Product Name</th>
+//                                 <th className="px-6 py-4">Price</th>
+//                                 <th className="px-6 py-4">Last Price</th>
+//                                 <th className="px-6 py-4">Stock</th>
+//                                 <th className="px-6 py-4">Description</th>
+//                                 <th className="px-6 py-4 text-center">Actions</th>
+//                             </tr>
+//                         </thead>
+//                         <tbody>
+//                             {products.map((product, index) => (
+//                                 <tr key={index} className="border-b hover:bg-blue-50 transition duration-300">
+//                                     <td className="px-6 py-4">{product.productId}</td>
+//                                     <td className="px-6 py-4">{product.productName}</td>
+//                                     <td className="px-6 py-4">Rs. {product.price}</td>
+//                                     <td className="px-6 py-4">Rs. {product.lastPrice}</td>
+//                                     <td className="px-6 py-4">{product.stock}</td>
+//                                     <td className="px-6 py-4">{product.description}</td>
+//                                     <td className="px-6 py-4 text-center flex gap-4 justify-center">
+//                                         <button className="text-red-600 hover:text-red-800"
+//                                             title="Delete"
+//                                             onClick={() => {
+//                                                 const token = localStorage.getItem('token');
+//                                                 axios.delete(`http://localhost:5000/api/products/${product.productId}`, {
+//                                                     headers: {
+//                                                         Authorization: `Bearer ${token}`,
+//                                                     },
+//                                                 }).then(() => {
+
+//                                                     toast.success("Product deleted successfully");
+//                                                     setProductLoaded(false);
+//                                                 });
+//                                             }}
+//                                         >
+//                                             <FaTrash />
+//                                         </button>
+//                                         <button className="text-blue-600 hover:text-blue-800">
+//                                             <FaPencil />
+//                                         </button>
+//                                     </td>
+//                                 </tr>
+//                             ))}
+//                         </tbody>
+//                     </table>

55d0b15034b580f05b7c18b655b46a1ed7c3d22c
gthilakshana <gavrawavanniarachchi@gmail.com>
2025-06-28 12:48:13 -0700

    Mobile Dropdown Updated 👍

--- src/components/header.jsx
+++ src/components/header.jsx
@@ -9,18 +9,18 @@ export default function Header() {
         <header className="bg-white dark:bg-gray-900 shadow-md w-full px-6 md:px-10 h-[80px] flex items-center justify-between sticky top-0 z-50 transition duration-300">
             {/* Logo */}
             <div className="flex items-center space-x-3">
-                <img
+                {/* <img
                     src="/logof.png"
                     alt="Logo"
                     className="h-12 w-12 rounded-full object-cover shadow-md cursor-pointer "
-                />
+                /> */}
                 <h1 className="text-2xl font-extrabold text-white hover:text-blue-400 transition duration-300 tracking-wide font-sans">
                     Mahee Fashion
                 </h1>
             </div>
 
             {/* Desktop Nav */}
-            <nav className="hidden md:flex space-x-8">
+            <nav className="hidden md:flex space-x-8  ">
                 {[
                     { path: "/", label: "Home" },
                     { path: "/product", label: "Products" },
@@ -65,18 +65,18 @@ export default function Header() {
 
             {/* Mobile Dropdown */}
             {menuOpen && (
-                <div className="absolute top-[80px] left-0 w-full bg-white dark:bg-gray-800 border-t border-gray-200 dark:border-gray-700 flex flex-col items-center md:hidden shadow-md z-40">
+                <div className="absolute top-[80px] right-0 w-60 h-screen bg-white dark:bg-gray-800 border-l border-gray-200 dark:border-gray-700 flex flex-col items-start md:hidden shadow-lg z-50">
                     {[
                         { path: "/", label: "Home" },
                         { path: "/product", label: "Products" },
                         { path: "/about", label: "About Us" },
-                        { path: "/contact", label: "Contact" }
+                        { path: "/contact", label: "Contact" },
                     ].map(({ path, label }) => (
                         <Link
                             key={path}
                             to={path}
                             onClick={() => setMenuOpen(false)}
-                            className="w-full text-center py-3 text-gray-700 dark:text-gray-300 hover:bg-blue-100 dark:hover:bg-blue-700 transition"
+                            className="w-full px-6 py-4 text-gray-700 dark:text-gray-300 hover:bg-gray-100 dark:hover:bg-gray-700 transition"
                         >
                             {label}
                         </Link>
